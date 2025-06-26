89ccc930ad31ae09e208e067159a7561707373ca
adeshkedlaya <adeshkedlaya03@gmail.com>
2025-06-26 12:00:24 +0530

    created a navbar for custom event calender

---
+++ src/components/navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const navbar=()=>{
+    return (
+        <nav class="navbar navbar-expand-lg bg-primary text-white">
+  <div class="container-fluid">
+    <a class="navbar-brand text-white" href="#">Custom Event Calender</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            View
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Monthly</a></li>
+            <li><a class="dropdown-item" href="#">Weekly</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-light" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    );
+};
+
+export default navbar;
