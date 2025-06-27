7bcd0f1dbff754ace4a74c737153f4787657fa8e
Adesh V  Kedlaya <103882185+adeshkedlaya05@users.noreply.github.com>
2025-06-27 12:32:27 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = ({ events, onFilter }) => {
   return (
     <nav className="navbar bg-primary text-white">
       <div className="container-fluid d-flex justify-content-between align-items-center">
-        <a className="navbar-brand text-white d-flex align-items-center" href="#">
-          <img src="calender-icon.png" alt="Calendar Icon" width="30" height="30" className="me-2" />
-          Custom Event Calender
-        </a>
-         {/* SearchBar component */}
+        <div className="navbar-brand text-white d-flex align-items-center">
+  <img src="calender-icon.png" alt="Calendar Icon" width="30" height="30" className="me-2" />
+  Custom Event Calendar
+</div>
+    {/* SearchBar component */}
         <SearchBar events={events} onFilter={onFilter} />
       </div>
     </nav>
